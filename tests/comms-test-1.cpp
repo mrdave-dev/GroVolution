69fbdedb7de35b72903ddd47b95ceb69700963fe
Dave Martinez <incubuddy@gmail.com>
2015-07-20 13:02:42 -0700

    Updating work from the weekend.

--- tests/comms-test-1.cpp
+++ tests/comms-test-1.cpp
@@ -12,6 +12,9 @@
  #include "../headers/commscpp.h"
 
  int main(int argc, char** argv) {
+ 	int testBaudRate = 38000;
+ 	int testPiPort = 14;
+
  	std::cout << "COMMS TEST 1\n";
  	std::cout << "\n";
  	
@@ -25,18 +28,15 @@
  	
 
  	std::cout << "Creating RS232Connection object, with args...\n";
- 	RS232Connection *secondConnect = new RS232Connection(38000, 14);
+ 	RS232Connection *secondConnect = new RS232Connection(testBaudRate, testPiPort);
 
 	if (secondConnect) {std::cout << "\nTEST PASSED: RS232Connection Created\n\n";}
  	else {std::cout << "\nTEST FAILED: RS232Connection did not initialize.\n\n";} 
 
  	std::cout << "firstConnect Addr.: " << firstConnect << "\n";
- 	std::cout << "secondConnect Addr.: " << secondConnect << "\n";
+ 	std::cout << "secondConnect Addr.: " << secondConnect << "\n\n";
 
  	std::cout << "02 - Get and set methods\n\n";
-
- 	int testBaudRate = 38000;
- 	int testPiPort = 14;
 
  	std::cout << "Testing unset rates...\n";
  	if (firstConnect->getBaudRate() == -1) {std::cout << "TEST PASSED: firstConnect baudrate == -1\n\n";}
@@ -46,6 +46,13 @@
  	if (firstConnect->getPiPort() == -1) {std::cout << "TEST PASSED: firstConnect piport == -1\n\n";}
  	else {std::cout << "TEST FAILED: firstConnect piport does not equal -1\n\n";};
 
+ 	std::cout << "Testing set baudrate\n";
+ 	if (secondConnect->getBaudRate() == testBaudRate) {std::cout << "TEST PASSED: secondConnect baudrate == 38000\n\n";}
+ 	else {std::cout << "TEST FAILED: secondConnect baudrate != 38000\n\n";};
+
+	std::cout << "Testing set piport...\n";
+ 	if (secondConnect->getPiPort() == testPiPort) {std::cout << "TEST PASSED: secondConnect piport == 14\n\n";}
+ 	else {std::cout << "TEST FAILED: secondConnect piport does not equal 14\n\n";};
 
 
 

e8e033e8c5574241c57b39ec0f8a967c221198e3
Dave Martinez <incubuddy@gmail.com>
2015-08-17 13:23:38 -0700

    Refactor done? Still gotta test it.

--- main.cpp
+++ main.cpp
@@ -21,6 +21,9 @@ void printHelp();
 
 
 int main(int argc, char** argv) {
+  // Create a new Bank
+  Bank* BankZero = nullptr;
+
     // Check for command line arguements
     if (argc > 1) {
         std::vector<std::string> arguements;
@@ -47,6 +50,7 @@ int main(int argc, char** argv) {
             temp.report();
 
             std::cout << "\n\nSUCCESS";
+            return 0;
 
         } else if (arguements[0] == "add") {
             Bank temp(arguements[1]);
@@ -58,9 +62,11 @@ int main(int argc, char** argv) {
             temp._save();
 
             std::cout << "\n\nSUCCESS";
+            return 0;
 
         } else if (arguements[0] == "delete") {
             std::cout << "lol no\n";
+            return 0;
 
         } else if (arguements[0] == "on") {
             Bank temp(arguements[1]);
@@ -78,6 +84,7 @@ int main(int argc, char** argv) {
             temp._save();
 
             std::cout << "\n\nSUCCESS";
+            return 0;
 
         } else if (arguements[0] == "off") {
             Bank temp(arguements[1]);
@@ -95,26 +102,32 @@ int main(int argc, char** argv) {
             temp._save();
 
             std::cout << "\n\nSUCCESS";
+            return 0;
 
         } else if (arguements[0] == "toggle") {
             std::cout << "lol no\n";
+            return 0;
+
+        } else if (arguements[0] == "load") {
+            BankZero = new Bank(arguements[1]);
 
         } else {
             std::cout << "\n\nERROR: Unknown operation\n";
             std::cout << "FAILURE\n\n";
+              return 0;
         }
-
-        // End before going in to the main program if a command
-        // line arguement is given.
-        return 0;
     }
 
     // PROGRAM SETUP
     std::cout << "... creating connection ...\n";
     RS232Connection* cx = new RS232Connection(BAUDRATE, PIPORT);
 
-    std::cout << "... creating empty bank ...\n";
-    Bank* BankZero = new Bank();
+    if (!BankZero) {
+      std::cout << "... creating empty bank ...\n";
+      BankZero = new Bank();
+    }
+
+    std::cout << "... setting bank connction ...\n";
     BankZero->_setConnection(cx);
 
     std::cout << "... almost done ...\n";
@@ -198,23 +211,18 @@ int main(int argc, char** argv) {
 void printHelp() {
 	std::cout << " GROVOLUTION CLI \n";
 	std::cout << " OPTIONS: \n";
-	std::cout << "\t add [char][#]\t\t- add a relay, i.e.: ./gv add P3 \n";
+	std::cout << "\t add [label]\t\t- add a relay, i.e.: ./gv add P3 \n";
 	std::cout << "\t report\t\t\t- show the current status of relays \n";
 	std::cout << "\t fetch\t\t\t- get the current status of the relays from the PLC \n";
-	std::cout << "\t switch [char][#]\t- turn a relay to its opposite status i.e.: ./gv switch P3\n";
-	std::cout << "\t rename [str]\t\t- Rename a bank. NOTE: name is used to save file.\n";
 	std::cout << "\t load [str]\t\t- Load a file, usually .json\n";
-	std::cout << "\t save\t\t\t- Save a file. NOTE: the bank's name is used as the filename\n";
-	std::cout << "\t on [char][#]\t\t- turn relay on\n";
-	std::cout << "\t off [char][#]\t\t- turn relay off\n";
-	std::cout << "\t all on\t\t\t- turn all relays on\n";
-	std::cout << "\t all off\t\t- turn all relays off\n";
-	std::cout << "\t invert\t\t\t- turns on switches on and off switches off\n";
+	std::cout << "\t save\t\t\t- Save a file.\n";
+	std::cout << "\t on [label | all]\t\t- turn relay on\n";
+	std::cout << "\t off [label | all]\t\t- turn relay off\n";
 	std::cout << "\t help\t\t\t- show this screen again\n";
 	std::cout << "\t quit\t\t\t- exit the program\n\n";
 
 	std::cout << " You may also this program with command line arguements: \n\n";
-	std::cout << "\t\tSYNTAX: ./gv [on|off|fetch] [filename] [relay label|all]\n\n";
+	std::cout << "\t\tSYNTAX: ./gv [action] [filename] [arguements]\n\n";
 
 	return;
 }

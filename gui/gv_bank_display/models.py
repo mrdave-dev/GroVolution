6d0f10219bad7a0a81351a2c3a12c64e9c4eac01
Dave Martinez <martind2@onid.oregonstate.edu>
2015-08-27 14:45:28 -0700

    Continuing work on GUI. Finished up with first part of Django tutorial.

--- gui/gv_bank_display/models.py
+++ gui/gv_bank_display/models.py
@@ -8,9 +8,15 @@ class GV_Bank(models.Model):
 	spray_interval = models.IntegerField()
 	spray_duration = models.IntegerField()
 	
+	def __str__(self):
+		return self.bank_name
+	
 
 class GV_Relay(models.Model):
 	bank = models.ForeignKey(GV_Bank)
 	label = models.CharField(max_length=2)
 	number = models.IntegerField()
 	status = models.BooleanField()
+
+	def __str__(self):
+		return str(self.label) + str(self.number)

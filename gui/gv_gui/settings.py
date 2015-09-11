c87b01fbda1305f3fd56ceb662fc0093c49b0973
Dave Martinez <martind2@onid.oregonstate.edu>
2015-09-11 05:29:07 +0000

    GUI wrapped up. Had to move contents of 'templates' in gui/ to app folder gui/gv_bank_display. Mergin back to master!

--- gui/gv_gui/settings.py
+++ gui/gv_gui/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r96wb7e724cen47e$62be$uh3&isu%8d)8mg2oxu3k95b@6(4-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.duckdns.org']
 
